68cf80a9c0efe4a2c9e94864c7fbdce67ea0a3e5
szehui-khoo <szehuikhoo97@gmail.com>
2025-03-24 10:32:47 +0800

    Random Objects Generator -- version 1.0

---
+++ ProgrammingChallenge_SZEHUI/ChallengeA/ChallengeA.cs
@@ -0,0 +1,102 @@
+﻿using System.Text;
+
+public class ChallengeA
+{
+	private static Random random = new Random();
+	
+	static void Main()
+	{
+		string rootFolder = Environment.CurrentDirectory;
+		// Navigate up 4 levels
+		if(!string.IsNullOrEmpty(rootFolder))
+		{
+			for (int i = 0; i < 4; i++)
+			{
+				rootFolder = Directory.GetParent(rootFolder).FullName;
+			}
+		}
+		
+		string filePath = Path.Combine(rootFolder, "RandomObjects.txt");
+		// 1KB = 1024 bytes (in binary)
+		long maxFileSize = 10 * 1024 * 1024;
+		long currentFileSize = 0;
+
+		using (StreamWriter streamWriter = new StreamWriter(filePath))
+		{
+			while (currentFileSize < maxFileSize)
+			{
+
+				string randomAlphabeticalString = GenerateAlphabeticalString(10);
+				string randomRealNumber = GenerateRealNumber();
+				string randomInteger = GenerateInteger();
+				string randomAlphanumeric = GenerateAlphanumeric(20);
+
+				string content = $"{randomAlphabeticalString},{randomRealNumber},{randomInteger},{randomAlphanumeric},";
+				streamWriter.Write(content);
+				currentFileSize += Encoding.UTF8.GetByteCount(content);
+			}
+
+			Console.WriteLine("The file 'RandomObjects.txt' has been generated with a size of 10MB.");
+		}
+	}
+
+	private static string GetAlphabets()
+	{
+		string alphabets = string.Empty;
+
+		// Uppercase letters
+		for (int u = 'A'; u <= 'Z'; u++)
+		{
+			alphabets = alphabets + ((char)u);
+		}
+
+		// Lowercase letters
+		alphabets += alphabets.ToLower();
+		return alphabets;
+	}
+
+	private static string GetNumbers()
+	{
+		string numbers = string.Empty;
+		for (int n = 0; n <= 9; n++)
+		{
+			numbers += n.ToString();
+		}
+		return numbers;
+	}
+
+	private static string GenerateAlphabeticalString(int length)
+	{
+		string alphabets = GetAlphabets();
+		string randomAlphabeticalString = string.Empty;
+		for (int i = 0; i < length; i++)
+		{
+			randomAlphabeticalString += alphabets[random.Next(alphabets.Length)];
+		}
+		return randomAlphabeticalString;
+	}
+
+	private static string GenerateRealNumber()
+	{
+		return random.NextDouble().ToString();
+	}
+
+	private static string GenerateInteger()
+	{
+		return random.NextInt64(0, 9999).ToString();
+	}
+
+	private static string GenerateAlphanumeric(int length)
+	{
+		string alphaNumerics = GetAlphabets() + GetNumbers();
+		string randomAlphaNumeric = string.Empty;
+		for (int i = 0; i < length; i++)
+		{
+			randomAlphaNumeric += alphaNumerics[random.Next(alphaNumerics.Length)];
+		}
+
+		int numberOfSpacesBefore = random.Next(0, 9);
+		int numberOfSpacesAfter = 10 - numberOfSpacesBefore;
+		return new string(' ', numberOfSpacesBefore) + randomAlphaNumeric + new string(' ', numberOfSpacesAfter);
+	}
+}
---
+++ ProgrammingChallenge_SZEHUI/ChallengeB/ChallengeB.cs
@@ -0,0 +1,91 @@
+﻿public class ChallengeB
+{
+	private enum Type { None, AlphabeticalString, RealNumber, Integer, Alphanumeric }
+
+	public static void Main()
+	{
+		// Challenge B
+		string inputFile = Path.Combine(GetDirectory(), "RandomObjects.txt");
+		string outputFile = Path.Combine(GetDirectory(), "OutputFile.txt");
+
+		// Challenge C
+		//string inputFile = "/app/RandomObjects.txt";
+		//string outputFile = "/app/OutputFile.txt";
+
+		if (File.Exists(inputFile))
+		{
+			var streamReader = new StreamReader(inputFile);
+			string content = content = streamReader.ReadToEnd();
+
+			// Print to console
+			string[] randomObjects = content.Split(',');
+
+			foreach (string rObject in randomObjects)
+			{
+				Type type = GetRandomObjectType(rObject);
+				Console.WriteLine(DisplayRandomObject(type, rObject.Replace(" ", "")));
+			}
+			
+			// Write to output file
+			var streamWriter = new StreamWriter(outputFile);
+			streamWriter.Write(content);
+			streamWriter.Close();
+		}
+		else
+		{
+			Console.WriteLine("RandomObjects.txt not found");
+		}
+	}
+
+	private static string GetDirectory()
+	{
+		string rootFolder = Environment.CurrentDirectory;
+		// Navigate up 4 levels
+		if (!string.IsNullOrEmpty(rootFolder))
+		{
+			for (int i = 0; i < 4; i++)
+			{
+				rootFolder = Directory.GetParent(rootFolder).FullName;
+			}
+		}
+		return rootFolder;
+	}
+
+	private static Type GetRandomObjectType(string content)
+	{
+		if (int.TryParse(content, out int number))
+		{
+			return Type.Integer;
+		}
+		else if (decimal.TryParse(content, out decimal realNumber))
+		{
+			return Type.RealNumber;
+		}
+		else if (content.Any(char.IsLetter) && !content.Any(char.IsDigit))
+		{
+			return Type.AlphabeticalString;
+		}
+		else if (content.Any(char.IsLetter) && content.Any(char.IsDigit))
+		{
+			return Type.Alphanumeric;
+		}
+		return default(Type);
+	}
+
+	private static string DisplayRandomObject(Type type, string content)
+	{
+		switch (type)
+		{
+			case Type.AlphabeticalString:
+				return $"Type : Alphabetical String  \n{content}";
+			case Type.RealNumber:
+				return $"Type : Real Number \n{content}";
+			case Type.Integer:
+				return $"Type : Integer \n{content}";
+			case Type.Alphanumeric:
+				return $"Type : Alphanumeric \n{content}";
+			default:
+				return string.Empty;
+		}
+	}
+}
